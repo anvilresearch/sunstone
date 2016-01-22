de5df10c0c4761280d80c04ea3c22540c25ba014
Greg Linklater <greglink49@gmail.com>
2016-01-22 23:11:05 +0200

    fix(PluginContainer): added necessary dependencies

--- core/arch/PluginContainer.js
+++ core/arch/PluginContainer.js
@@ -1,7 +1,18 @@
 'use strict'
 
+/**
+ * Local Dependencies
+ */
+var Plugin = require('./Plugin')
+
+/**
+ * Symbols
+ */
 const injector = Symbol()
 
+/**
+ * PluginContainer
+ */
 class PluginContainer {
 
   constructor (name, manifest, _injector) {
@@ -15,9 +26,17 @@ class PluginContainer {
   }
 
   load () {
-    if (!this.factory) throw new Error(`Plugin ${this.name} does not have an initializer callback`)
-    return this.factory(new Plugin(this.name, this.manifest))
+    if (!this.factory) {
+      throw new Error(`Plugin ${this.name} does not have an initializer callback`)
+    }
+    
+    let plugin = new Plugin(this.name, this.manifest, this[injector])
+    this.factory(plugin)
+    return plugin
   }
 }
 
+/**
+ * Exports
+ */
 module.exports = PluginContainer
