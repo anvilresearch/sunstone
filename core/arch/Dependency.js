bb7349f8fc2acdd2e60415c4949a3ca4a425c30b
Christian Smith <smith@anvil.io>
2016-01-26 15:04:00 -0800

    fix(dependency): extractDependencies handles multiple dependency types

--- core/arch/Dependency.js
+++ core/arch/Dependency.js
@@ -34,8 +34,21 @@ class Dependency extends Model {
    * extractDependencies
    */
   extractDependencies () {
-    if (this.factory && !descriptor.dependencies) {
-      this.dependencies = Dependency.extractDependencies(this.factory)
+    // TODO
+    // consider a generalized name for the function on a dependency
+    // and see if we can eliminate this kind of overly specific code
+    let lookup = {
+      'factory': 'factory',
+      'alias': 'factory',
+      'adapter': 'factory',
+      'module': 'factory',
+      'extension': 'mutator',
+      'callback': 'callback',
+    }
+
+    let fn = this[lookup[this.type]]
+    if (fn  && !this.dependencies) {
+      this.dependencies = Dependency.extractDependencies(fn)
     }
   }
 
@@ -49,24 +62,24 @@ class Dependency extends Model {
     const factoryErrorMessage = 'A factory, mutator, callback or value is required'
     function factoryConform (value, instance) {
       // check for a value on the dependency
-      if (this.value) {
+      if (instance.value) {
         return true
       }
 
       // if there is no value present there needs to be a factory, callback or mutator
       // and any of those need to be a function
-      let present = this.factory || this.callback || this.mutator
-      return present 
-        ? typeof present === 'function' 
-        || this.value 
+      let present = instance.factory || instance.callback || instance.mutator
+      return present
+        ? typeof present === 'function'
+        || instance.value
         : false
     }
 
     // seeing as the property would be the same for all factory types and value;
     // create a single instance of the property for use over all four.
-    let factoryConformProperty = { 
-      type: 'any', 
-      conform: factoryConform, 
+    let factoryConformProperty = {
+      type: 'any',
+      conform: factoryConform,
       messages: {
         conform: factoryErrorMessage
       }
