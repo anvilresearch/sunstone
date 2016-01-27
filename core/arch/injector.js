8df090f7c9ff420c2b3508eda5ccaf51523fa7d0
Greg Linklater <greglink49@gmail.com>
2016-01-27 02:00:19 +0200

    refactor(injector): added injector singleton

---
+++ core/arch/injector.js
@@ -0,0 +1,16 @@
+'use strict'
+
+/**
+ * Injector
+ */
+const Injector = require('./Injector')
+
+/**
+ * Singleton
+ */
+const injector = new Injector()
+
+/**
+ * Exports
+ */
+module.exports = injector
