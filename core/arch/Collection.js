94a4562c04ddbb40cd7c4dc5fbec366562f48d1d
Greg Linklater <greglink49@gmail.com>
2016-01-31 01:13:14 +0200

    docs(Collections): jsdoc formatting

--- core/arch/Collection.js
+++ core/arch/Collection.js
@@ -2,16 +2,28 @@
 
 /**
  * Dependencies
+ * @ignore
  */
 const _ = require('lodash')
 
 /**
  * Collection
+ * @class
+ * A collection of values with some additional logic in the form
+ * of the ```.filter()``` method. Used as a generic for
+ * {@link PluginCollection} and {@link DependencyCollection}
+ * 
+ * @param {collection} collection Base collection
+ *
+ * @extends Array
  */
 class Collection extends Array {
 
   /**
    * Constructor
+   *
+   * @description
+   * Initialize a new collection.
    */
   constructor (collection) {
     super()
@@ -23,6 +35,14 @@ class Collection extends Array {
 
   /**
    * Filter
+   *
+   * @description
+   * Filter the current collection and return a new collection
+   * (of the same type) containing items matching the predicate.
+   * 
+   * @param {object | function} predicate Lodash-style predicate
+   * 
+   * @returns {Collection}
    */
   filter (predicate) {
     return new this.constructor(_.filter(this, predicate))
--- core/arch/PluginCollection.js
+++ core/arch/PluginCollection.js
@@ -2,17 +2,30 @@
 
 /**
  * Dependencies
+ * @ignore
  */
 const _ = require('lodash')
 const Collection = require('./Collection')
 
 /**
  * PluginCollection
+ * 
+ * @class
+ * Results container for results of querying plugins from the registry.
+ *
+ * @param {collection} collection Base collection
+ *
+ * @extends Collection
+ * 
  */
 class PluginCollection extends Collection {
 
   /**
    * Start
+   * 
+   * @description
+   * Iterate over plugins in the collection and call ```plugin.start()```
+   * on each.
    */
   start () {
     this.forEach(plugin => plugin.start())
