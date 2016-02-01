d264169632e67579dd146be62c79e3a71657cbb6
Greg Linklater <greglink49@gmail.com>
2016-02-01 11:27:46 +0200

    test(injector): stub Injector tests

---
+++ test/unit/core/arch/InjectorSpec.js
@@ -0,0 +1,68 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const cwd = process.cwd()
+const path = require('path')
+const chai = require('chai')
+const sinon = require('sinon')
+const sinonChai = require('sinon-chai')
+
+/**
+ * Assertions
+ */
+chai.use(sinonChai)
+chai.should()
+let expect = chai.expect
+
+/**
+ * Code under test
+ */
+const Injector = require(path.join(cwd, 'core', 'arch', 'Injector'))
+
+/**
+ * Tests
+ */
+describe('Injector', () => {
+  describe('constructor', () => {
+    it('should initialize dependencies')
+    it('')
+  })
+
+  describe('register', () => {
+    it('should register the dependency on the injector')
+  })
+
+  describe('get', () => {
+    describe ('with an invalid dependency', () => {
+      it('should throw an error')
+    })
+
+    describe('with a valid dependency', () => {
+      describe('and the dependency has been fetched before', () => {
+        it('should return the dependency')
+      })
+
+      describe('and the dependency has not been fetched yet', () => {
+        it('should invoke the dependency\'s factory function')
+        it('should return the dependency value')
+      })
+      
+    })
+    
+  })
+
+  describe('invoke', () => {
+    describe('with an invalid dependency name', () => {
+      it('should do nothing')
+    })
+
+    it('should invoke a callback on the injector')
+  })
+
+  describe('filter', () => {
+    it('should return a new DependencyCollection')
+  })
+
+})
